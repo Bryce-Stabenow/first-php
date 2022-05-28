85b6bddfaeb5afba55a3886ae5e10d04283186bf
Bryce Stabenow <brycestabenow617@gmail.com>
2022-05-28 13:48:20 -0700

    exploring floats, ints, and booleans

--- action.php
+++ action.php
@@ -12,6 +12,32 @@
     <?php endif; ?>
 </h2>
 
+<hr>
+
 <?php
-echo gettype($_POST['name'])
+$name_type = gettype($_POST['name']);
+$bignumber = 111_234_567;
+$number_info = var_dump($bignumber);
+
+if ($name_type == "string"){
+    echo 'Name variable type: string';
+} else {
+    echo $number_info;
+    echo 'Why is your name a number?';
+}
+?>
+
+<hr>
+
+<?php
+function paperwork(int $n, int $m): int
+{
+    if ($n < 0 || $m < 0 || $n == NAN || $m == NAN) {
+        return 0;
+    } else {
+        return $n * $m;
+    }
+}
+
+echo paperwork(5, 10);
 ?>
